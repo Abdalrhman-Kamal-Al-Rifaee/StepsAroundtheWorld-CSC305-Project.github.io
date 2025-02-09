cff7a4c383c18bdded8fed6a8ec67b9494cab696
Abdalrhman-Kamal-Al-Rifaee <alrifaee.2002@gmail.com>
2025-02-09 15:39:03 +0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
-// // Form Validation
-// document.getElementById('contactForm').addEventListener('submit', function(event) {
-//     event.preventDefault(); // Prevent form submission
+// Form Validation
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
 
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const message = document.getElementById('message').value;
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
 
-//     if (name && email && message) {
-//         alert('Thank you for contacting us!');
-//         // You can add code here to send the form data to a server
-//     } else {
-//         alert('Please fill out all fields.');
-//     }
-// });
+    if (name && email && message) {
+        alert('Thank you for contacting us!');
+        // You can add code here to send the form data to a server
+    } else {
+        alert('Please fill out all fields.');
+    }
+});
 
-// //form Continent
-// function redirectToDestination(select) {
-//     var selectedValue = select.value;
-//     window.location.href = "destinations.html" + selectedValue;
-// }
+//form Continent
+function redirectToDestination(select) {
+    var selectedValue = select.value;
+    window.location.href = "destinations.html" + selectedValue;
+}
